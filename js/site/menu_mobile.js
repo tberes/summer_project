5d9ef7a0af6dd8fe11d835c498a380670b46fee5
mohosb <mohosb@gmail.com>
2017-07-06 16:01:30 +0200

    Added sliding animation for the menu on mobile. Added menu point and some css changes.

--- js/site/menu_mobile.js
+++ js/site/menu_mobile.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function(){
+    var ph = 0;
     $("#menu-btn").click(function(){
-        console.log("asd");
+        if(ph == 0){
+            $("#menu-wrapper").animate({left: "65px"}, "1000");
+            ph = 1;
+        }
+        else if(ph == 1){
+            $("#menu-wrapper").animate({left: "100%"}, "1000");
+            ph = 0;
+        }
     });
 });
